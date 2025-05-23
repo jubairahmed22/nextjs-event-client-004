667c6acc122c453d451f79f0eb66872f5fdc5e5b
jubairahmed22 <jubairahmed060@gmail.com>
2025-05-23 18:02:44 +0600

    something is added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,6 +9,7 @@ import NavbarDynamicSidebar from "@/components/shared/NavbarDynamicSidebar";
 import { FilterProvider } from "./contexts/FilterContext";
 import NavbarDynamicSidebarAllProduct from "@/components/shared/NavbarDynamicSidebarAllProduct";
 import NavbarMenu from "@/components/shared/NavbarMenu";
+import Script from "next/script";
 
 export const metadata: Metadata = {
   title: "My Color Events",
@@ -24,6 +25,18 @@ export default async function RootLayout({
     <html lang="en" data-theme="light">
     <head>
       {/* Add the Google Fonts link here */}
+       <Script
+          strategy="afterInteractive"
+          src={`https://www.googletagmanager.com/gtag/js?id=G-9JN7TW10E6`}
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-9JN7TW10E6');
+          `}
+        </Script>
       <link
         rel="stylesheet"
         href="https://fonts.googleapis.com/css2?family=Marcellus&family=Jost:wght@400;700&family=Roboto:wght@400;700&family=Space+Mono&family=Montserrat&family=Courier+Prime&family=Poppins:wght@400;700&family=Raleway:wght@400;700&family=Nunito+Sans:wght@400;700&family=Playfair+Display:wght@400;700&family=Lora:wght@400;700&family=Abril+Fatface&family=Pacifico&display=swap"
